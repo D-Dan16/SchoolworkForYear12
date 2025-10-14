0f75c576761d08975d542ab3b571af620efcbe75
DirtyDan16 <138337745+DirtyDan16@users.noreply.github.com>
2025-10-14 23:47:33 +0300

    Recursion, p.33+ [v.2]

--- SchoolworkForYear12/Work/Recursion/Classwork-29_09_25/ArrayQuestions.cs
+++ SchoolworkForYear12/Work/Recursion/Classwork-29_09_25/ArrayQuestions.cs
@@ -9,14 +9,25 @@ public static class ArrayQuestions
 
     private static void Start()
     {
-        int[] arr = [4,6,7,8,9,10];
-        SumElementsUntil(8, arr);
+        CalcSumOfSequence(5);
 
-        CountPositiveElementsUntilIndex(4, arr);
+        CalcSumOfSequenceWithRadical(5);
+
+        CalcSquareOfFibonacci(5);
+
+        int[] arr = [4,6,7,8,9,10];
+
+        SumElementsUntil(4, arr);
+
+        SumElementsFrom(4, arr);
+
+        CountPositiveElementsUntilIndex(3, arr);
 
         IsArraySorted(arr);
 
-        IsNotContainingPrimes(arr);
+        FindIndexOfElement(arr, 8);
+
+        HasPrimeInArray(arr);
 
         int[,] arr2D = new[,]
         {
@@ -25,108 +36,15 @@ public static class ArrayQuestions
             {7, 8, 9}
         };
 
-        CountNumIn2DArray(arr2D,5,arr2D.GetLength(0)-1);
+        IsMatBiggerThan(arr2D, 3);
 
-        //TODO: EX20
+        CountNumIn2DArray(arr2D, 4, arr2D.GetLength(0) - 1);
 
-        const string str = "This is a STRING, that ALSO contains 12345";
-        CountLowerCaseInString(str);
 
-        PrintDividers(16);
-
-        PrintElements(7, 3, 5);
-
-        PrintAlternatingSequence(10);
-
-        //TODO: rest
 
     }
 
-    /**
-     * ex30
-     */
-    private static void PrintAlternatingSequence(int numOfElements)
-    {
-        if (numOfElements == 0)
-            return;
-
-        PrintAlternatingSequence1(4,0);
-        return;
-
-        void PrintAlternatingSequence1(int curNum, int index)
-        {
-            if (index == numOfElements)
-                return;
-
-            Console.WriteLine(curNum);
-
-            var offset = index % 2 == 0 ? -1 : 2;
-            var newNum = curNum + offset;
-
-            PrintAlternatingSequence1(newNum, index + 1);
-        }
-    }
-
-    /**
-     * ex28
-     */
-    private static void PrintElements(int curElement, int distanceBetweenElements, int numOfElementsToPrint)
-    {
-        if (numOfElementsToPrint == 0)
-            return;
-
-        Console.WriteLine(curElement);
-
-        PrintElements(curElement+distanceBetweenElements, distanceBetweenElements, numOfElementsToPrint-1);
-    }
-
-    /**
-     * ex25
-     */
-    private static void PrintDividers(int num)
-    {
-        if (num is 1 or 2)
-            return;
-
-        PrintDividers(num,2);
-        return;
-
-        static void PrintDividers(int num, int divisor)
-        {
-            if (divisor*2 >= num)
-                return;
-
-            PrintDividers(num, divisor+1);
-
-            if (num % divisor == 0)
-                Console.WriteLine(divisor);
-        }
-    }
-
-    // ReSharper disable once UnusedMethodReturnValue.Local
-    /**
-     * ex21
-     */
-    private static int CountLowerCaseInString(string str)
-    {
-        return CountLowerCaseInString(str, 0);
-
-        static int CountLowerCaseInString(string str, int curCharIndex)
-        {
-            if (curCharIndex >= str.Length)
-                return 0;
-
-            int incrementIfIsLowerCase = str[curCharIndex] >= 97 && str[curCharIndex] <= 122 ? 1 : 0;
-
-            return
-                incrementIfIsLowerCase +
-                CountLowerCaseInString(str, curCharIndex + 1);
-        }
-    }
-
-    /**
-     * ex19
-     */
+    /** ex30 */
     private static int CountNumIn2DArray(int[,] arr2D, int lookedForNum, int row)
     {
         if (row < 0)
@@ -148,33 +66,89 @@ public static class ArrayQuestions
         }
     }
 
+
     /**
-     * ex18
+     * ex28
      */
-    private static bool IsNotContainingPrimes(int[] arr,int index = 0)
+    private static bool IsMatBiggerThan(int[,] arr2D, int comparedVal)
     {
-        if (index >= arr.Length - 1)
+        return IsMatBiggerThan(arr2D, comparedVal, 0);
+    }
+
+    private static bool IsMatBiggerThan(int[,] arr2D, int lookedForNum, int row)
+    {
+        if (row > arr2D.GetLength(0))
             return true;
 
-        if (!IsPrime(arr[index]))
+        if (!IsRowBiggerThan(lookedForNum))
             return false;
 
-        return IsNotContainingPrimes(arr, index + 1);
+        return IsMatBiggerThan(arr2D, lookedForNum, row+1);
 
-        bool IsPrime(int integer,int divisor = 2)
+        bool IsRowBiggerThan(int comparisonValue, int elementNum = 0)
         {
-            if (divisor >= Math.Sqrt(integer))
+            if (elementNum > arr2D.GetLength(1))
                 return true;
 
-            if (integer % divisor == 0)
+            if (arr2D[row, elementNum] < comparisonValue)
                 return false;
 
-            return IsPrime(integer, divisor + 1);
+            return IsRowBiggerThan(comparisonValue, elementNum + 1);
         }
     }
 
     /**
-     * ex17
+     * ex25
+     */
+    private static bool HasPrimeInArray(int[] arr)
+    {
+        return HasPrimeInArray(arr, 0);
+    }
+
+    private static bool HasPrimeInArray(int[] arr, int index)
+    {
+        if (index > arr.Length - 1)
+            return false;
+
+        if (IsPrime(arr[index]))
+            return true;
+
+        return HasPrimeInArray(arr, index+1);
+    }
+
+    private static bool IsPrime(int integer,int divisor = 2)
+    {
+        if (divisor >= Math.Sqrt(integer))
+            return true;
+
+        if (integer % divisor == 0)
+            return false;
+
+        return IsPrime(integer, divisor + 1);
+    }
+
+    /**
+     * ex22
+     */
+    private static int FindIndexOfElement(int[] arr, int element)
+    {
+        return FindIndexOfElement(arr, element,0);
+    }
+
+    private static int FindIndexOfElement(int[] arr, int element, int index)
+    {
+        if (index > arr.Length - 1)
+            return -1;
+
+        if (arr[index] == element)
+            return index;
+
+        return FindIndexOfElement(arr, element, index+1);
+    }
+
+
+    /**
+     * ex21
      */
     private static bool IsArraySorted(int[] arr, int index = 0)
     {
@@ -187,21 +161,95 @@ public static class ArrayQuestions
         return IsArraySorted(arr, index + 1);
     }
 
+
     /**
-     * ex15
-     */
+    * ex20
+    */
     private static int CountPositiveElementsUntilIndex(int index, int[] arr)
     {
         if (index < 0 || index >= arr.Length) return 0;
         return (arr[index] > 0 ? 1 : 0) + CountPositiveElementsUntilIndex(index - 1, arr);
     }
 
+
     /**
-     * ex14
+     * ex19
+     */
+    private static int SumElementsFrom(int index, int[] arr)
+    {
+        if (index < 0 || index >= arr.Length) return 0;
+        return arr[index] + SumElementsFrom(index + 1, arr);
+    }
+
+    /**
+     * ex18
      */
     private static int SumElementsUntil(int index, int[] arr)
     {
         if (index < 0 || index >= arr.Length) return 0;
         return arr[index] + SumElementsUntil(index - 1, arr);
+    }
+
+
+    /**
+     * ex17
+     */
+    private static int CalcSquareOfFibonacci(int index)
+    {
+        switch (index)
+        {
+            case 1:
+                return 0;
+            case 2:
+                return 1;
+        }
+
+        return CalcSquareOfFibonacci(index, 3, 1, 1, 0);
+    }
+
+    private static int CalcSquareOfFibonacci(
+        int goalIndex,
+        int curIndex,
+        int curValue,
+        int valueBefore,
+        int valueBefore2Times
+    ) {
+        if (curIndex == goalIndex)
+            return valueBefore;
+
+        valueBefore2Times = valueBefore;
+        valueBefore = curValue;
+        curValue = (int)(Math.Pow(valueBefore2Times, 2) + Math.Pow(valueBefore, 2));
+
+        return CalcSquareOfFibonacci(goalIndex, curIndex+1, curValue, valueBefore, valueBefore2Times);
+    }
+
+    /**
+     * ex15
+     */
+    private static int CalcSumOfSequenceWithRadical(int num)
+    {
+        if (num == 1)
+            return 1;
+
+        int addedValue = (int)(num % 2 == 0 ?
+            -Math.Sqrt(2*num-1) :
+            2*num-1);
+
+        return addedValue + CalcSumOfSequenceWithRadical(num - 1);
+
+    }
+
+    /**
+     * ex14
+     */
+    private static int CalcSumOfSequence(int num)
+    {
+        if (num == 1)
+            return 2;
+
+        int addedValue = num % 2 == 1 ? num * 2 : (int)Math.Pow(num, 2);
+
+        return addedValue + CalcSumOfSequence(num - 1);
     }
 }
