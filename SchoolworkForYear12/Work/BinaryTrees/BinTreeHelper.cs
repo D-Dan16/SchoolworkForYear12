73a3a5129f143584f5eff66ad02d00a79c2ac9b2
DirtyDan16 <138337745+DirtyDan16@users.noreply.github.com>
2026-01-05 00:49:18 +0200

    Campus IL stuff - Binary Trees

---
+++ SchoolworkForYear12/Work/BinaryTrees/BinTreeHelper.cs
@@ -0,0 +1,68 @@
+﻿namespace SchoolworkForYear12.Work.BinaryTrees;
+
+public static class BinTreeHelper
+{
+    public static BinNode<T> BuildIdent<T>(BinNode<T> bt)
+    {
+        if (bt == null)
+            return null;
+
+        return new BinNode<T>(
+            BuildIdent(bt.GetLeft()),
+            bt.GetValue(),
+            BuildIdent(bt.GetRight())
+        );
+    }
+
+    public static BinNode<int> BuildTreeFromArrays(int[] preorder, int[] inorder)
+    {
+        int i = 0;
+        BinNode<int> bt = new BinNode<int>(preorder[0]);
+        while (preorder[0] != inorder[i])
+            i++;
+        if (i > 0)
+        {
+            int[] preorderL = new int[i];
+            int[] inorderL = new int[i];
+            for (int j=0; j<i; j++)
+            {
+                preorderL[j] = preorder[j+1];
+                inorderL[j] = inorder[j];
+            }
+            bt.SetLeft(BuildTreeFromArrays(preorderL,inorderL));
+        }
+        if (i < inorder.Length-1)
+        {
+            int[] preorderR = new int [inorder.Length-1-i];
+            int[] inorderR = new int[inorder.Length-1-i];
+            for (int j=i+1; j<inorder.Length; j++)
+            {
+                preorderR[j-i-1] = preorder[j];
+                inorderR[j-i-1] = inorder[j];
+            }
+            bt.SetRight(BuildTreeFromArrays(preorderR,inorderR));
+        }
+        return bt;
+    }
+
+    public static int CountTreeHeight<T>(BinNode<T> bt)
+    {
+        if (bt == null)
+            return -1;
+
+        return Math.Max(
+            GoLevelDeeper(0,bt.GetLeft()),GoLevelDeeper(0,bt.GetRight())
+        );
+
+        int GoLevelDeeper(int level, BinNode<T> node)
+        {
+            if (node == null)
+                return level;
+
+            return Math.Max(
+                GoLevelDeeper(level+1,node.GetLeft()),GoLevelDeeper(level+1,node.GetRight())
+            );
+        }
+    }
+
+}
---
+++ SchoolworkForYear12/Work/BinaryTrees/CampusILChapter4.cs
@@ -0,0 +1,40 @@
+﻿namespace SchoolworkForYear12.Work.BinaryTrees;
+
+public static class CampusILChapter4
+{
+    public static void Start()
+    {
+        var tree1 = BinTreeHelper.BuildTreeFromArrays([1,2,3,2,9,5], [3,2,2,1,9,5]);
+        PrintAllNumbers(tree1);
+    }
+
+
+    public static void PrintAllNumbers(BinNode<int> bt)
+    {
+        if (bt == null)
+            return;
+
+        if (!bt.HasLeft() && !bt.HasRight())
+            Console.Write(bt.GetValue());
+
+        MoveToNextStep(bt.GetLeft(), bt.GetValue());
+        MoveToNextStep(bt.GetRight(), bt.GetValue());
+
+        void MoveToNextStep(BinNode<int> node, int summationOfDigits)
+        {
+            if (node == null)
+                return;
+
+            summationOfDigits = summationOfDigits * 10 + node.GetValue();
+
+            if (!node.HasLeft() && !node.HasRight())
+            {
+                Console.Write($"{summationOfDigits} ");
+                return;
+            }
+
+            MoveToNextStep(node.GetLeft(), summationOfDigits);
+            MoveToNextStep(node.GetRight(),summationOfDigits);
+        }
+    }
+}
